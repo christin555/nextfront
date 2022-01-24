3b71955fccb5ca9d04ddb58489f50f916f4bfe0e
Крис <kriska_555@mail.ru>
2022-01-24 22:04:24 +0500

    *

--- components/HeadComponent.js
+++ components/HeadComponent.js
@@ -1,28 +1,29 @@
 import Head from 'next/head'
-const Meta = (props) => (
-    <Head>
+
+export default function Meta(props) {
+   return  <Head>
         <title>{props.title}</title>
-        <meta name="description" content={props.desc} />
-         {
+        <meta name="description" content={props.desc}/>
+        {
             props.image &&
-                <meta property="og:image" content={`${props.image}`} />
+            <meta property="og:image" content={`${props.image}`}/>
         }
         {
             props.canonical &&
-            <link rel="canonical" href={`${props.canonical}`} />
+            <link rel="canonical" href={`${props.canonical}`}/>
         }
         {
             props.js &&
             <script type="text/javascript" src={`${props.js}`}></script>
         }
         {
-             props.breadcumbs &&
-             <script type="application/ld+json"
-                     dangerouslySetInnerHTML={{
-                             __html: JSON.stringify(props.breadcumbs)
-                     }}>
-             </script>
+            props.breadcumbs &&
+            <script type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify(props.breadcumbs)
+                    }}>
+            </script>
         }
     </Head>
-)
-export default Meta
+}
+
