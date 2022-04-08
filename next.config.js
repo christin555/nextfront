c3c2258df27b66df7a7a882582ceee91c1a0bfa1
Крис <kriska_555@mail.ru>
2022-04-08 17:01:54 +0500

    2

--- next.config.js
+++ next.config.js
@@ -2,4 +2,27 @@ module.exports = {
     images: {
         domains: ['https://master-pola.com', 'master-pola.com']
     },
+    async headers() {
+        return [
+            {
+                source: '/:all*(svg|jpg|png)',
+                locale: false,
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+                    }
+                ],
+            },
+            {
+                source: '/about',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+                    }
+                ],
+            },
+        ]
+    },
 }
