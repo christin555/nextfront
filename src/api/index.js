aa055993ab94a0d12b29b5d96044622492ea0355
Крис <kriska_555@mail.ru>
2023-03-04 22:58:55 +0500

    up

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `/api`;
+const API_URL = `https://api.master-pola.com/api/`;
 const baseQuery = axios.create({
     baseURL: API_URL,
     responseType: 'json',
