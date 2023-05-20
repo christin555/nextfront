83f9344b71919be68590040c2aa9f36d117e9da7
Крис <kriska_555@mail.ru>
2023-05-20 14:35:06 +0500

    add banner

--- components/pages/catalog/Content/Banner.js
+++ components/pages/catalog/Content/Banner.js
@@ -33,7 +33,7 @@ const items = [
     textButton: 'Подробнее'
   },
   {
-    background: 'https://master-pola.com/dashboard/uploads/alpinefloor_desc_0_75x_91e78189f2.png',
+    background: 'https://master-pola.com/dashboard/uploads/alpinefloor_desc_0_99x_a79a010223.jpg',
     mobileBackground: 'https://master-pola.com/dashboard/uploads/alpine_flor_mob_0_75x_c81dc5dc8e.png'
   },
   {
