7d52a8b26fd15aa97f775f2c23721c7b63e3a51a
Крис <kriska_555@mail.ru>
2024-01-04 14:11:55 +0500

    del режим работы

--- components/Menu/menu.js
+++ components/Menu/menu.js
@@ -52,8 +52,8 @@ export default function Menu() {
     <React.Fragment>
       <header className={styles.container}>
         <div className={styles.header}>
-          <MobileHeader />
-          <Garland />
+          <MobileHeader/>
+          <Garland/>
           {/*<div className={styles.headerBackground}/>*/}
           <div className={styles.headerContent}>
             <div className={styles.logoBlock}>
@@ -65,22 +65,22 @@ export default function Menu() {
                   className={styles.logo}
                   src={'/logo.png'}
                   alt={'logo'}
-                  width='64'
-                  height='64'
+                  width="64"
+                  height="64"
                 />
               </div>
               <div className={styles.name} onClick={() => toPage('/')}>
-                                Мастер Пола
+                Мастер Пола
               </div>
               <a
                 target={'_blank'}
-                rel='noopener noreferrer'
+                rel="noopener noreferrer"
                 className={styles.address}
                 href={'https://2gis.ru/tyumen/firm/70000001041302673?m=65.569066%2C57.099076%2F16'}
-                title='Показать адрес в 2GIS'
+                title="Показать адрес в 2GIS"
               >
-                <NearMeIcon className={styles.icon} />
-                                Тюмень, Мельникайте 138а
+                <NearMeIcon className={styles.icon}/>
+                Тюмень, Мельникайте 138а
               </a>
             </div>
             <div className={styles.left}>
@@ -88,9 +88,9 @@ export default function Menu() {
                 <a
                   href={'tel:+79829881522'}
                   className={styles.phone}
-                  title='Позвонить'
+                  title="Позвонить"
                 >
-                                    8 982 988-15-22
+                  8 982 988-15-22
                 </a>
                 <Callme ButtonCall={(
                   <span className={styles.callme}>
@@ -100,16 +100,13 @@ export default function Menu() {
                 />
               </div>
             </div>
-            <BurgerMenu toPage={toPage} menu={menu} />
+            <BurgerMenu toPage={toPage} menu={menu}/>
           </div>
-        </div>
-        <div className={g.text}>
-          Режим работы в праздничные дни: 31 декабря, 1 - 3 января - <b> выходные дни </b>
         </div>
       </header>
       <nav className={styles.menuContainer}>
         <ul className={styles.menu}>
-          <li><InputSearch /></li>
+          <li><InputSearch/></li>
           {
             menu.map(({name, link, Icon}, index) => (
               <li key={link} className={cn({[styles.isActive]: link === pathname})}>
@@ -117,7 +114,7 @@ export default function Menu() {
                   href={link}
                   title={name}
                 >
-                  {Icon ? <Icon className={styles.icon} /> : null} {name}
+                  {Icon ? <Icon className={styles.icon}/> : null} {name}
                 </a>
               </li>
             ))
@@ -127,50 +124,50 @@ export default function Menu() {
           <li>
             <a
               target={'_blank'}
-              rel='noopener noreferrer'
+              rel="noopener noreferrer"
               href={`https://wa.me/79829881522`}
-              title='Написать в WhatsApp'
+              title="Написать в WhatsApp"
             >
-              <WhatsAppIcon className={styles.icon} />
+              <WhatsAppIcon className={styles.icon}/>
             </a>
           </li>
           <li>
             <a
               target={'_blank'}
-              rel='noopener noreferrer'
-              href='https://www.instagram.com/masterpola72'
-              title='Перейти на канал в Instagram'
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/masterpola72"
+              title="Перейти на канал в Instagram"
             >
-              <InstagramIcon className={styles.icon} />
+              <InstagramIcon className={styles.icon}/>
             </a>
           </li>
           <li>
             <a
               target={'_blank'}
-              rel='noopener noreferrer'
+              rel="noopener noreferrer"
               href={`viber://chat?number=%289829881522`}
             >
-              <ViberIcon className={styles.icon} />
+              <ViberIcon className={styles.icon}/>
             </a>
           </li>
           <li>
             <a
               target={'_blank'}
-              rel='noopener noreferrer'
-              href='https://vk.com/masterpola'
-              title='Перейти в сообщество ВКонтакте'
+              rel="noopener noreferrer"
+              href="https://vk.com/masterpola"
+              title="Перейти в сообщество ВКонтакте"
             >
-              <VkIcon className={styles.icon} />
+              <VkIcon className={styles.icon}/>
             </a>
           </li>
           <li>
             <a
               target={'_blank'}
-              rel='noopener noreferrer'
-              href='https://www.youtube.com/channel/UC86lrjiQpHt0Fwzr0FoBxyA'
-              title='Перейти на канал в YouTube'
+              rel="noopener noreferrer"
+              href="https://www.youtube.com/channel/UC86lrjiQpHt0Fwzr0FoBxyA"
+              title="Перейти на канал в YouTube"
             >
-              <YouTubeIcon className={styles.icon} />
+              <YouTubeIcon className={styles.icon}/>
             </a>
           </li>
         </ul>
